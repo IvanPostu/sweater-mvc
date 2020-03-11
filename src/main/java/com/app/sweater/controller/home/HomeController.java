674c21ed0b2a55aae426d37505f173b478e5b815
IvanPostu <ipostu20000127@gmail.com>
2020-03-11 18:20:03 -0400

    added images for messages

--- src/main/java/com/app/sweater/controller/home/HomeController.java
+++ src/main/java/com/app/sweater/controller/home/HomeController.java
@@ -4,22 +4,30 @@ import com.app.sweater.domain.Message;
 import com.app.sweater.domain.User;
 import com.app.sweater.persistence.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class HomeController {
 
+  @Value("${upload.path}")
+  private String uploadPath;
+
   @Autowired
   MessageRepository messageRepository;
 
-  @GetMapping("/")
+  @GetMapping
   public String home(Model model){
 
     Iterable<Message> messages = messageRepository.findAll();
@@ -28,14 +36,31 @@ public class HomeController {
     return "index";
   }
 
-  @PostMapping("/")
+  @PostMapping
   public String add(
       @AuthenticationPrincipal User user,
       @RequestParam String text,
       @RequestParam String tag,
-      Model model){
+      @RequestParam("file") MultipartFile file,
+      Model model) throws IOException {
 
     Message message = new Message(text, tag, user);
+
+    if (file != null && !file.getOriginalFilename().isEmpty()) {
+      File uploadDir = new File(uploadPath);
+
+      if (!uploadDir.exists()) {
+        uploadDir.mkdir();
+      }
+
+      String uuidFile = UUID.randomUUID().toString();
+      String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+      file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+      message.setFilename(resultFilename);
+    }
+
     messageRepository.save(message);
 
     Iterable<Message> messages = messageRepository.findAll();
